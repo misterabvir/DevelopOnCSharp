7d06327306384615f378f717f0ac98f153300dc2
misterabvir <to@misterabvir.ru>
2023-11-28 21:02:14 +0300

    s2

---
+++ HM2/Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using BitLogic;
+
+long longValue = 1234567890L;
+Bits bitLongValue = longValue;
+Console.WriteLine($"Long = {longValue} => Bits = {bitLongValue}");
+long longValueFromBits = (long)bitLongValue;
+Console.WriteLine($"Bits = {bitLongValue} => Long = {longValueFromBits}");
+/*
+Long = 1234567890 => Bits = 1001001100101100000001011010010
+Bits = 1001001100101100000001011010010 => Long = 1234567890
+*/
+
+int intValue = 123456789;
+Bits bitIntValue = intValue;
+Console.WriteLine($"Integer = {intValue} => Bits = {bitIntValue}");
+int intValueFromBits = (int)bitIntValue;
+Console.WriteLine($"Bits = {bitIntValue} => Integer = {intValueFromBits}");
+/*
+Integer = 123456789 => Bits = 111010110111100110100010101
+Bits = 111010110111100110100010101 => Integer = 123456789
+*/
+
+byte byteValue = 123;
+Bits bitByteValue = byteValue;
+Console.WriteLine($"Byte = {byteValue} => Bits = {bitByteValue}");
+byte byteValueFromBits = (byte)bitByteValue;
+Console.WriteLine($"Bits = {bitByteValue} => Byte = {byteValueFromBits}");
+/*
+Byte = 123 => Bits = 1111011
+Bits = 1111011 => Byte = 123
+*/
