e95e77e84b05ff3cb099905818bfe1f37ac47151
misterabvir <to@misterabvir.ru>
2023-12-15 23:17:38 +0300

    homework7

---
+++ HM7/CustomNameAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace HM7;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+internal class CustomNameAttribute(string customPropertyName) : Attribute
+{
+    public string CustomPropertyName => customPropertyName;
+}
